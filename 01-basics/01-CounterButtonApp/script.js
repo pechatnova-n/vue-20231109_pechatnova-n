12a752014c35f43a3f03812ec906b0a76ebee069
Natalia Pechatnova <pechatnova.n@yandex.ru>
2023-11-11 20:10:37 +0300

    создание vue-приложения и добавление кнопки-счетчика

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,10 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+}).mount("#app")
