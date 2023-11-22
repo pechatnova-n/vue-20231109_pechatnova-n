967f117e8083892ffa94cc10940abdff0a3091d7
Natalia Pechatnova <pechatnova.n@yandex.ru>
2023-11-22 20:19:01 +0300

    добавление кнопки-счетчика

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,8 +2,26 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      required: true,
+      default: 0,
+    }
+  },
+
+  emits: ['update:count'],
+
+  methods: {
+    incrCount(count) {
+      count = ++count;
+      this.$emit('update:count', count);
+    },
+  },
+
+
 
   // Компонент должен иметь входной параметр и порождать событие
 
-  template: `<button type="button">1</button>`,
+  template: `<button @click=incrCount(count) type="button">{{ count }}</button>`,
 });
