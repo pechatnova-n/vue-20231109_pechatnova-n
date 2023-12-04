45abc74655645fa32e0243a6d44b0c9201156e9b
Natalia Pechatnova <pechatnova.n@yandex.ru>
2023-12-04 09:37:15 +0300

    реализация компонента UiFormGroup

--- 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
+++ 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="form-group">
+  <div class="form-group" :class="{'form-group_inline' : inline}">
     <!-- form-group_inline -->
-    <label class="form-group__label">label text</label>
+    <label v-if="label" class="form-group__label">{{ label }}</label>
     <!-- CONTENT -->
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
   name: 'UiFormGroup',
+
+  props: {
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+  }
+
+
 };
+
+
+
 </script>
 
 <style scoped>
