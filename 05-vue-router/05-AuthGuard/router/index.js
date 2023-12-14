6ea45d5e796521f557b946d565d08ee2e46ba54b
Natalia Pechatnova <pechatnova.n@yandex.ru>
2023-12-14 12:25:46 +0300

    добавление AuthGuard

--- 05-vue-router/05-AuthGuard/router/index.js
+++ 05-vue-router/05-AuthGuard/router/index.js
@@ -6,6 +6,89 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'index',
+      alias: '/meetups',
+      component: () => import('../views/PageMeetups.vue'),
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      meta: {
+        requireGuest: true,
+      },
+      component: () => import('../views/PageLogin.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: {
+        requireGuest: true,
+      },
+      component: () => import('../views/PageRegister.vue'),
+    },
+
+    {
+      path: '/meetups/create',
+      meta: {
+        requireAuth: true,
+      },
+      component: () => import('../views/PageCreateMeetup.vue'),
+    },
+    {
+      path: '/meetups/:meetupId(\\d+)/edit',
+      meta: {
+        requireAuth: true,
+      },
+      component: () => import('../views/PageEditMeetup.vue'),
+    },
+  ],
+
+});
+
+router.beforeEach(async(to, from) => {
+   if (to.meta.requireGuest && isAuthenticated()) {
+    return {
+      name: 'index',
+    }
+  } else if (to.meta.requireAuth && !isAuthenticated()) {
+    return {
+       name: 'login',
+       query: { from: to.fullPath },
+     }
+  }
+})
+
+export { router };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { createRouter, createWebHistory } from 'vue-router';
+import { isAuthenticated } from '../services/authService.js';
+
+const router = createRouter({
+  history: createWebHistory('/05-vue-router/05-AuthGuard'),
+  routes: [
+    {
+      path: '/',
       alias: '/meetups',
       component: () => import('../views/PageMeetups.vue'),
     },
@@ -40,4 +123,4 @@ const router = createRouter({
   ],
 });
 
-export { router };
+export { router };*/
