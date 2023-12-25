1942230bbc2a1de5ab8cbfa816b24d58d43d0c59
Natalia Pechatnova <pechatnova.n@yandex.ru>
2023-12-25 10:20:05 +0300

    добавление компонента-обертки для checkbox

--- 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
+++ 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
@@ -1,14 +1,36 @@
 <template>
   <label class="checkbox">
-    <input type="checkbox" class="checkbox__input" />
+    <input
+      v-model="modelProxyValue"
+      type="checkbox"
+      class="checkbox__input"
+      v-bind="$attrs"
+    />
     <span class="checkbox__box"></span>
-    Label Text
+    <slot />
   </label>
 </template>
 
 <script>
 export default {
   name: 'UiCheckbox',
+
+  inheritAttrs: false,
+
+  props: {
+    modelValue: [Boolean, Array, Set],
+  },
+
+  computed: {
+    modelProxyValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    },
+  },
 };
 </script>
 
