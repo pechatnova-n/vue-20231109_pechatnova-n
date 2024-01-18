52c5eab24baf4bed76076dff6a180705345e74a9
Natalia Pechatnova <pechatnova.n@yandex.ru>
2024-01-18 21:48:27 +0300

    reactify

--- 08-reactivity/05-reactify/utils/reactify.js
+++ 08-reactivity/05-reactify/utils/reactify.js
@@ -1,4 +1,4 @@
-import { computed } from 'vue';
+import { computed, unref } from 'vue';
 
 /**
  * @template T
@@ -6,6 +6,5 @@ import { computed } from 'vue';
  * @returns {function(...[*]): ComputedRef<T>} - Функция вычисления от ref-ов, возвращающая вычисляемое значение computed
  */
 export function reactify(func) {
-  // ...
-  // return () => computed(() => {});
+   return (...args) => computed(() => func(...args.map(a => unref(a))));
 }
